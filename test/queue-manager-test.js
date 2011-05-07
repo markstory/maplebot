887fe8b3e6c820890a2ace3970a535f36a00e944
Mark Story <mark@mark-story.com>
2011-05-06 23:29:24 -0400

    Refactoring QueueManager. Pulling out a base class for use with other tasks. Adding vows for QueueManager.

---
+++ test/queue-manager-test.js
@@ -0,0 +1,36 @@
+var QueueManager = require('../lib/queue-manager').QueueManager,
+	vows = require('vows'),
+	assert = require('assert');
+
+vows.describe('QueueManager Task').addBatch({
+	'adding to the queue': {
+		topic: function () {
+			return new QueueManager();
+		},
+
+		'works when there is texts': function(topic) {
+			var response = topic.add('1234');
+			assert.equal('Hotfix queue is: [1234]', response);
+
+			response = topic.add('567');
+			assert.equal('Hotfix queue is: [1234, 567]', response);
+		},
+
+		'trims space off': function (topic) {
+			var response = topic.add('   89  ');
+			assert.equal('Hotfix queue is: [1234, 567, 89]', response);
+		}
+	},
+	'removing from the queue': {
+		topic: function () {
+			return new QueueManager();
+		},
+		'shortens the list': function (topic) {
+			topic.add('1234');
+			var response = topic.remove();
+			assert.equal('The Hotfix queue is empty, well done!', response);
+		}
+
+	}
+
+}).export(module);
