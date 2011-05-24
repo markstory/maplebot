e79f08fc92e595e11efeebfdafc54966726e792c
Mark Story <mark@mark-story.com>
2011-05-23 20:09:33 -0400

    Adding per/room task bindings.  You can bind only some tasks to a specific room.  If you leave off the rooms configuration, then tasks will be attached to all rooms.

--- test/bot-test.js
+++ test/bot-test.js
@@ -8,19 +8,16 @@ vows.describe('Bot').addBatch({
 			var bot = new Bot({});
 		}, Error);
 	},
-
 	'missing task module causes error': function () {
 		assert.throws(function () {
 			var bot = new Bot({tasks: { 'Kaboom': '/path/to/nowhere'}});
 		}, Error);
 	},
-
 	'missing task object causes error': function () {
 		assert.throws(function () {
 			var bot = new Bot({tasks: { 'Kaboom': '../lib/queue-manager'}});
 		}, Error);
 	},
-
 	'configuration': {
 		topic: function () {
 			var config = {
@@ -28,10 +25,9 @@ vows.describe('Bot').addBatch({
 			};
 			return new Bot(config);
 		},
-
 		'binds commands': function (topic) {
-			assert.ok(topic.commands['!qadd']);
-			assert.ok(topic.commands['!qls']);
+			assert.ok(topic.commands.QueueManager['!qadd']);
+			assert.ok(topic.commands.QueueManager['!qls']);
 		},
 		'stores a reference to task objects': function (topic) {
 			assert.ok(topic.tasks[0], 'Missing task');
@@ -39,24 +35,69 @@ vows.describe('Bot').addBatch({
 			assert.ok(topic.tasks[0].bindCommands);
 		}
 	},
-
 	'message delegation': {
 		topic: function () {
 			var bot = new Bot({tasks: {}});
-			bot.commands['!test'] = function (text) {
+			bot.commands.Test = {};
+			bot.commands.Test['!test'] = function (text) {
 				return text;
 			};
 			return bot;
 		},
-
 		'finds task commands': function (topic) {
-			var result = topic.handleMessage('!test hello world');
-			assert.equal('hello world', result);
+			var result = topic.handleMessage({ body: '!test hello world'});
+			assert.ok(result.body);
+			assert.equal('hello world', result.body);
 		},
-
 		'returns empty on miss': function (topic) {
-			var result = topic.handleMessage('!swing and a miss');
+			var result = topic.handleMessage({body: '!swing and a miss'});
 			assert.equal('', result);
+		}
+	},
+	'multi-room task handling': {
+		topic: function () {
+			var config = {
+				tasks: {
+					'QueueManager': '../lib/queue-manager',
+					'Help': '../lib/help'
+				},
+				rooms: {
+					'test1': ['QueueManager'],
+					'test2': ['Help']
+				}
+			};
+			var bot = new Bot(config);
+			return bot;
+		},
+		'tasks not in a room will not respond to messages': function (topic) {
+			var request = {
+				room: 'test1',
+				body: '!help'
+			};
+			var resp = topic.handleMessage(request);
+			assert.equal(resp, '', 'Help task is not bound in room test1');
+
+			request = {
+				room: 'test2',
+				body: '!qls'
+			};
+			resp = topic.handleMessage(request);
+			assert.equal(resp, '');
+		},
+		'tasks in a room will respond to messages': function (topic) {
+			var request = {
+				room: 'test1',
+				body: '!qls'
+			};
+			var resp = topic.handleMessage(request);
+			assert.ok(/Hotfix/.test(resp.body), 'Should have replied');
+
+			request = {
+				room: 'test2',
+				body: '!help'
+			};
+			resp = topic.handleMessage(request);
+			assert.ok(/Available Commands/.test(resp), 'Help should have displayed');
 		}
 	}
 
