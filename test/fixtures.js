72e40cda54a3a3243969ac25a1c9b874b0be8a7e
Mark Story <mark@freshbooks.com>
2011-07-19 10:34:04 -0400

    Adding fixture file missed in last commit. Updating readme.

---
+++ test/fixtures.js
@@ -0,0 +1,10 @@
+var ConfigTest = function (bot, config) {
+	console.log(config);
+	this.config = config;
+};
+
+ConfigTest.prototype.bindCommands = function (commands) {
+
+};
+
+exports.ConfigTest = ConfigTest;
